601edd5da712b52a384911d0375aeb05382d240e
TheMrClaus <github@spamcatching.com>
2026-05-19 16:36:35 +0200

    release: 0.5.49-beta-app-tv

--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.omnio.tv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 99
-        versionName = "0.5.48-beta"
+        versionCode = 100
+        versionName = "0.5.49-beta"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${releaseValue("PARENTAL_GUIDE_API_URL")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${releaseValue("INTRODB_API_URL")}\"")
